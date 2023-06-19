db5e9191ae730ca840e171d42640338eafa9ad2a
catalfacu <cataldof21@gmail.com>
2023-06-19 02:10:24 -0300

    Creando base de datos y conectando con el back

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+   id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+   },
+   nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+   },
+   descripcion: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+   },
+   plataformas:{
+    type: DataTypes.ENUM('PlayStation','Xbox','Nintendo','PC','Mobile','Other'),
+    defaultValue: 'Other',
+    allowNull: false,
+   },
+   imagen: {
+    type: DataTypes.STRING,
+    allowNull: true,
+   },
+   fecha_de_lanzamiento: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+   },
+   rating: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+      max: 5,
     },
+   },
   });
 };
