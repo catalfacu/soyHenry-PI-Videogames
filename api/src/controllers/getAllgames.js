8ca3192858547005ebbbe31ee0db39a54ef672a0
catalfacu <cataldof21@gmail.com>
2023-06-21 17:49:13 -0300

    Backend: Ruta get/names lista

--- api/src/controllers/getAllgames.js
+++ api/src/controllers/getAllgames.js
@@ -1,11 +1,28 @@
-const {infoDB, getApiInfo} = require('../utils/index');
+const axios = require('axios');
+const {infoDB, getApiInfo, gameByNameDb, gamesInfo} = require('../utils/index');
+const URL= "https://api.rawg.io/api/games?search="
+const {API_KEY} = process.env;
+
+//TODO: NI BIEN SE TENGA TIEMPO REFACTORIZAR EL CODIGO! 
 
 const getAllGames = async(req,res) => {
     try {
+        const { name } = req.query;
+        if(name) {                                                                //Si existe name en la url busca tanto en DB como en la api y guarda los primeros 15 resultados en un array y lo devuelve
+        const gamesDb = await gameByNameDb(name);
+        const {data: {results} } = await axios (`${URL}${name}&key=${API_KEY}`);
+        const gamesApi = results.map(game => gamesInfo(game));
+        const allGames = [...gamesDb,...gamesApi];
+        const first15Games = allGames.slice(0,15);                                                                      
+        return res.status(200).json(first15Games);
+
+        } else {
+
         const apiGames = await getApiInfo();
         const gamesDB = await infoDB();
-        const allGames = apiGames.concat(gamesDB);
+        const allGames = [...gamesDB,...apiGames];                  //Si no existe name en la url, muestra todos los juegos tanto de DB como de la Api
         return res.status(200).json(allGames);
+        }
         
     } catch (error) {
         return res.status(500).json({err: error.message});
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const getAllGames = require('../controllers/getAllgames');
 const getGameById = require('../controllers/getGameId');
-const getGamesbyName = require('../controllers/getGameByName');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -12,11 +12,10 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 //GET /videogames
+//GET /videogames/name?="..."
 router.get("/videogames", getAllGames);
 //GET /videogames/:id
 router.get("/videogames/:id" , getGameById)
-//GET /videogames/name?="..."
-router.get("/videogames/?name=", getGamesbyName);
 //POST /videogames
 //GET /genres
 module.exports = router;
