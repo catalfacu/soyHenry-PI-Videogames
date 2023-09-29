75db8b7b490fb340f4b35d16e4c923fbd74e8073
catalfacu <cataldof21@gmail.com>
2023-09-29 19:02:49 -0300

    Creeacion de los componentes Login y Register

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import styles from './Login.module.css';
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.LoginRegister}>
+        <section className={styles.forms}>
+          <h1>Accede a tu cuenta</h1>
+          <form>
+            <label>
+              e-mail
+              <input type="email" required />
+            </label>
+            <label>
+              contraseña
+              <input type="password" required/>
+            </label>
+            <button>acceder</button>
+          </form>
+        </section>
+
+        <section className={styles.forms}>
+          <h1>Registrate</h1>
+          <form>
+            <label>
+              Nombre de Usuario
+              <input type="text" />
+            </label>
+            <label>
+              Edad
+              <input type="number" />
+            </label>
+            <label>
+              Contraseña
+              <input type="password" />
+            </label>
+            <label>
+              Confirme la contraseña
+              <input type="password" />
+            </label>
+            <button>Registrate</button>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Login
