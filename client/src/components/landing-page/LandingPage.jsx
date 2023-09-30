1cec47a9a69c6c74e114852979f5700094934bf1
catalfacu <cataldof21@gmail.com>
2023-09-29 23:33:01 -0300

    Ajuste de renderizado de elemento login y responsive

--- client/src/components/landing-page/LandingPage.jsx
+++ client/src/components/landing-page/LandingPage.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import styles from './landingPage.module.css';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import Login from '../Login/Login';
 
 
 function LandingPage(props) {
@@ -38,17 +39,20 @@ function goHome() {
             <div className={styles.textPresentation}>
            <img src="https://fontmeme.com/permalink/230701/4c15be69eeb4dd1df9eaead2c7758433.png" alt="fuente-grand-theft-auto" border="0"/>
             </div>
+            {
+                loginOK && <Login/>
+            }
             <div className={styles.button}>
              <button className={styles.landingButton} onClick={goHome}>START!</button>
             </div>
             
             <div className={styles.aboutMe}>
             <img src="https://fontmeme.com/permalink/230701/bc677451588e40819330a9718ea3f3b5.png" alt="fuente-grand-theft-auto" border="0"/>
-            </div>
-            <div className={styles.socialNetworks}>
+            <span className={styles.socialNetworks}>
                 <a href="https://www.instagram.com/facucata23/?hl=es-la" target='blank'>IG</a>
                 <a href="https://github.com/catalfacu" target='blank'>Github</a>
                 <a href="https://www.linkedin.com/in/facundo-cataldo-9a0031272/" target='blank'>Linkedln</a>
+            </span>
             </div>
         </div>
     );
