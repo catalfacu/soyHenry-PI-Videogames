7081a55d6e90532337c433eff77d36c346005354
catalfacu <cataldof21@gmail.com>
2023-10-10 18:28:19 -0300

    Ajustes para conectar registro de usuarios entre front y back

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,31 +1,6 @@
-import {useState,useRef, useEffect}from 'react'
 import styles from './Login.module.css';
 
-function Login({modalVisible,setModalVisible}) {
-
-  //const [modalVisible, setModalVisible] = useState(false);
-  // const modalRef = useRef(null);
-
-  // useEffect(() => {
-  //   // Función para cerrar el modal cuando se hace clic fuera de él
-  //   function handleClickOutside(event) {
-  //     if (modalRef.current && !modalRef.current.contains(event.target)) {
-  //       // El clic ocurrió fuera del modal, cierra el modal aquí
-  //       setModalVisible(false);
-  //     }
-  //   }
-
-  //   // Agrega un oyente de eventos de clic al documento cuando el modal está visible
-  //   if (modalVisible) {
-  //     console.log('first')
-  //     document.addEventListener("click", handleClickOutside);
-  //   }
-
-  //   // Limpia el oyente de eventos cuando el componente se desmonta
-  //   return () => {
-  //     document.removeEventListener("click", handleClickOutside);
-  //   };
-  // }, [modalVisible]);
+function Login() {
 
   return (
     <div className={styles.container}>
@@ -49,12 +24,24 @@ function Login({modalVisible,setModalVisible}) {
           <h1>Registrate</h1>
           <form>
             <label>
-              Nombre de Usuario
+              Nombre
               <input type="text" />
             </label>
             <label>
-              Edad
-              <input type="number" />
+              Apellido
+              <input type="text" />
+            </label>
+            <label>
+              Cumpleaños
+              <input type="date" />
+            </label>
+            <label>
+              Nombre de Usuario
+              <input type= "text" />
+            </label>
+            <label>
+              E-mail
+              <input type="email" />
             </label>
             <label>
               Contraseña
