53f9d22420c7aae61aea32f0fa2d461fa4460e92
Facundo Cataldo <cataldof21@gmail.com>
2025-07-23 19:48:41 -0300

    Ajustes de actualizacion

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { reducer } from "./reducer";
-import thunkMiddleware from "redux-thunk";
+import { thunk as thunkMiddleware } from "redux-thunk";
+
+
+
 
 // Permite utilizar "REACT-REDUX":
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
